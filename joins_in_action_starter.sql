b06c7e46e37bfc8185b7f7a8003ade9d2a556846
Xaivntaaj Her <dn_her@yahoo.com>
2023-04-19 11:58:04 -0500

    finished problems and created ERD and placed in folder, ready to push

--- joins_in_action_starter.sql
+++ joins_in_action_starter.sql
@@ -11,30 +11,49 @@ SELECT * FROM movies RIGHT JOIN users on movie_id = favorite_movie_id;
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 1 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a left join to see all records from the movies table and matching records from the users table.
 -- EXPECTED RESULT: 75 Rows Returned
-
+select * from movies
+left join users
+on movie_id = user_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 2 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a right join to see all records from the users table and matching records from the movies table.
 -- EXPECTED RESULT: 16 Rows Returned
-
+select * from movies
+right join users
+on movie_id = user_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 3 >>>>>>>>>>>>>>>>>>>>>>>
 -- Do a inner join to get only the rows from the movies tables that have matching rows in the users tables.
 -- EXPECTED RESULT: 14 Rows Returned
-
+select * from movies
+inner join users
+on movie_id = favorite_movie_id;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 4 >>>>>>>>>>>>>>>>>>>>>>>
 -- Determine which lead studio's movies are favorited by users the most.
 -- EXPECTED RESULT: Disney
-
+select lead_studio from movies
+order by audience_score desc
+limit 1;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 5 >>>>>>>>>>>>>>>>>>>>>>>
 -- Get the average Rotten Tomatoes score of all movies that are favorited by a user.
 -- EXPECTED RESULT: 52.21
-
+select avg(rotten_tomatoes) from movies
+inner join users
+on movie_id = favorite_movie_id
+where user_id is not null;
 
 -- <<<<<<<<<<<<<<<<<<<<<< PROBLEM 6 >>>>>>>>>>>>>>>>>>>>>>>
 -- Determine which two movies are have duplicate entries in the dataset. (HINT: No join is needed for this problem)
 -- EXPECTED RESULT: Mamma Mia!, Gnomeo and Juliet
 -- It is important to be able to identify dirty data within a dataset!
+select film, count(*) from movies
+group by film
+having count(*) > 1;
+
+
+
+
+
 
